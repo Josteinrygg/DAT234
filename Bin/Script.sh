0b58aa9dba2e30327d7ca2bc54df337466a5fb6b
Jostein Våga Rygg <Joster17@uia.no>
2019-08-28 17:05:23 +0200

    Fjernet If setning

--- Bin/Script.sh
+++ Bin/Script.sh
@@ -6,10 +6,4 @@ for IP in $(seq 1 255);
   do
     ping -c 1 $SUBNET.$IP
 
-if  [[ "Packets Sent:" == "Packets Sent"]]; then
-    echo "This IP is pinged"
-  else
-    echo "Did not connect to host"
-fi
-
 done
