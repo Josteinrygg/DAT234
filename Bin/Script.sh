e900abbbb7390e9d18757242918c6955c5b4d6a3
Henrik Lindseth <henrl17@uia.no>
2019-08-28 22:47:54 +0200

    IP scanner fix

--- Bin/Script.sh
+++ Bin/Script.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 echo "Please Enter the subnet"
 read SUBNET
-
+touch adress.txt
+echo $adress > adress.txt
+c=0
+touch replied.txt
+echo "Ip's which replied: " > replied.txt
 for IP in $(seq 1 255);
-  do
-    ping -c 1 $SUBNET.$IP
-
+do
+if ping -c 1 -w 1 $SUBNET.$IP &> /dev/null
+then
+	echo "Reply"
+	echo "$SUBNET.$IP" >> replied.txt
+	else
+	echo "Miss"
+	fi
 done
+cat replied.txt
