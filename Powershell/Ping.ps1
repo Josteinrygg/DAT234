e6860d805d03cddd346c47cbdb97bdaafdb76226
Henrik Lindseth <henrl17@uia.no>
2019-09-23 12:10:55 +0200

    Update Ping.ps1
    fixed pingsweep

--- Powershell/Ping.ps1
+++ Powershell/Ping.ps1
@@ -1,1 +1,27 @@
-1..255 | % {"192.168.100.$($_): $(Test-Connection -count 1 -comp 192.168.100.$($_) -quiet)"}
+Write-Host "Choose function"
+
+[int]$EType = read-host "Please type 1 to ping range 192.168.100.15/24 or type 2 for pingsweep!"
+
+if ($EType -eq 1)
+{
+$a = 15
+$z = 24
+while ($a -le $z) {
+    Test-Connection -computername "192.168.100.$a" -Quiet -count 1
+        $a++
+} 
+
+"Checked range from 15 to 24!"
+
+break
+
+}
+
+if ($EType -eq 2)
+{
+    0..254 | % {"192.168.100.$($_): $(Test-Connection -count 1 -comp 192.168.100.$($_) -quiet)"}
+
+"Pingsweep in subnet complete!"
+}
+
+
