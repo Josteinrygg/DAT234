330deea90323144dcc4a4415c44cb5049e170866
Henrik Lindseth <henrl17@uia.no>
2019-09-27 16:55:22 +0200

    Create PingRetryNo9000.ps1
    Forsøk på oppgave 3

---
+++ Powershell/PingRetryNo9000.ps1
@@ -0,0 +1,21 @@
+﻿Write-host "Ping IP address"
+
+$subnet = read-host "Choose an IP in which to ping"
+
+$pattern = "^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$"
+
+
+if ($subnet -match $pattern) {
+
+   0..254 | % {"$subnet.$($_): $(Test-Connection -count 1 -comp $subnet.$($_) -quiet 1)"}break
+}
+
+else
+
+{
+"`n ERROR! Please try with a valid IP address."
+}
+
+#Dette er så langt jeg kommer... Koden går igjennom hele subnet, men får feil på parameter -computername (comp).
+#Aner ikke hvordan jeg skal skrive for at pipen faktisk skal sjekke connection på inputen. Står "Argument is null or empty": altså -comp.
+#Koden skjønner om inputen er en IP adresse (IPv4) eller ikke da, som er ganske nice! Skjekker inputen opp imot angitt pattern.
