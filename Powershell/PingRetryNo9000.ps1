33a2ec15fa40993c0ca3f0cc760e0320bfd2b39b
Henrik Lindseth <henrl17@uia.no>
2019-09-27 20:35:45 +0200

    Update PingRetryNo9000.ps1
    hjelp

--- Powershell/PingRetryNo9000.ps1
+++ Powershell/PingRetryNo9000.ps1
@@ -1,11 +1,12 @@
 ﻿Write-host "Ping IP address"
 
-$IP = read-host "Choose an IP in which to ping"
+[IPAddress]$IP = read-host "Choose an IP in which to ping"
+
 
 # En simpel konverter jeg fant som ikke ser ut til å fungere i dette scriptet (blir kluss med if-statement):
 
-#[int]$returnedInt = 0
-#[bool]$result = [int]::TryParse($IP, [ref]$IPInt)
+#[int]$IP_int = 0
+#[bool]$result = [int]::TryParse($IP, [ref]$IP_int)
 
 
 $pattern = "^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$"
@@ -23,5 +24,5 @@ else
 }
 
 # Dette scriptet printer input som string, og må konverteres til int32 for å fungere.
-# Output blir IP adressen + 0-254, istedenfor å gå igjennom hele subnet X.X.X.0-254.
+# Output blir IP adressen + 0-254, istedenfor å gå igjennom hele subnet X.X.X.___
 # Den sjekker inputen opp imot pattern for IP adresser, og sender en feilmelding dersom inputen i read-host ikke er en IPv4 adresse.
