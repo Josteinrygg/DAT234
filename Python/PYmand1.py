e63fe38eda35e9da01d914258715c25ccd9c62ff
Jostein Våga Rygg <Joster17@uia.no>
2019-10-07 11:01:26 +0200

    Task1 complete(?)

---
+++ Python/PYmand1.py
@@ -0,0 +1,23 @@
+import requests
+import re
+
+
+def task1():
+    r = requests.get('https://links.datapor.no')
+    print(r.text)
+
+def task2():
+    re.sub('(http|https)://[\w-]+(.[\w-]+)+\S*')
+
+
+
+
+
+
+
+
+
+
+
+
+
