1d76bf235d969db4808e8ca10fc2586be368fa48
Joakim <joakimsch@hotmail.com>
2019-10-10 12:41:05 +0200

    Start task6

--- Python/Mandatory Python Scripting.py
+++ Python/Mandatory Python Scripting.py
@@ -1,18 +1,19 @@
 import requests
 import re
 
-def task5():
 
-    r = requests.get('https://links.datapor.no')
-    test_str = r.text
-    string = str(test_str)
-    list = re.findall('[<][a-zA-Z0-9]+[\s]|[<][a-zA-Z0-9]+[>]', string)
+def task1():
+
+    r = requests.get('https://tech.co/')
+    text = r.text
+    string = str(text)
+    list = re.findall('title>(.+?)</title', text)
     s = set()
 
     for i in list:
-        i = re.split('[<|>]', i)
-        s.add(i[1])
+        i = re.split('[.]', i)
+        s.add(i[-1])
 
     print(s)
 
-task5()
+task1()
