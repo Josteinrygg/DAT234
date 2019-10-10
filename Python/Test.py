37930e7a994129f424788988450fead353b5df01
Jostein Våga Rygg <Joster17@uia.no>
2019-10-10 11:51:12 +0200

    TLD TESt activate

--- Python/Test.py
+++ Python/Test.py
@@ -2,4 +2,11 @@ import requests
 import re
 
 r = requests.get('https://links.datapor.no')
-print(help(r))
+test_str = r.text
+string = str(test_str)
+matches = re.findall(r'(.com)', string)
+output = []
+for x in matches:
+    if x not in output:
+        output.append(x)
+print(output)
