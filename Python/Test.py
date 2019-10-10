2613ab84c81790dd0f6b6cb35848e22960aaf31f
Henrik Lindseth <henrl17@uia.no>
2019-10-10 14:47:49 +0200

    finale

--- Python/Test.py
+++ Python/Test.py
@@ -1,5 +1,3 @@
-import requests
-import re
 
 r = requests.get('https://tech.co')
 text = r.text
@@ -214,23 +212,5 @@ text = r.text
 l52 = re.findall('title>(.+?)</title', text)
 
 
-
-
-
 print(l1, l2, l3, l4, l5, l6, l7,l8, l9, l10, l11, l12, l13, l14, l15, l16, l17, l18, l19, l20, l21, l22, l23, l24, l26, l27, l28, l29, l30, l31, l32, l33, l34, l35, l36, l37, l38, l39, l40, l41, l42, l43, l44, l45, l46, l47, l48, l49, l50, l51, l52)
 
-"""
-4'https://fonts.googleapis.com', 5'https://techcrunch.com', 6'https://www.vice.com',
-7'https://www.theregister.co.uk', 8'https://blog.hackeriet.no', 9'https://refsdalolsen.com',
-10'https://blog.jessfraz.com', 11'https://carolynvanslyck.com', 12'http://blog.disapped.com',
-13'https://hugo.md', 14'https://blog.jonasbengtson.se', 15'https://manfred.life', 16'https://www.kode24.no',
-17'https://cxsecurity.com', 18'http://netsec.ws', 19'https://malicious.link', 20'https://blog.0day.rocks', 21'https://www.schneier.com',
-22'https://www.matteomalvica.com', 23'https://canyouseeme.org', 24'https://www.howsmyssl.com',
-25'https://tools.pingdom.com', 26'https://dnsdumpster.com', 27'https://repl.it', 28'https://gchq.github.io',
-29'http://fcc.io', 30'http://exif.regex.info', 31'https://www.yr.no', 32'https://bgp.he.net', 33'https://censys.io',
-34'https://crt.sh', 35'https://ghostproject.fr', 36'https://godbolt.org', 37'https://coolors.co',
-38'https://palettegenerator.com', 39'https://leovoel.github.io', 40'https://flexboxfroggy.com',
-41'https://bolig.sia.no', 42'https://uia.instructure.com', 43'https://fsweb.no', 44'https://cloud.timeedit.net',
-45'https://e5.onthehub.com', 46'https://video.uia.no', 47'http://cair.datapor.no',
-48'http://blog.disapped.com', 49'http://netsec.ws', 50'http://fcc.io', 51'http://exif.regex.info', 52'http://cair.datapor.no'
-"""
