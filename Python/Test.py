2237eed10c13fea53c40a46a467665e88867c0d1
Henrik Lindseth <henrl17@uia.no>
2019-10-10 14:39:46 +0200

    l25 removed

--- Python/Test.py
+++ Python/Test.py
@@ -101,9 +101,9 @@ r = requests.get('https://www.howsmyssl.com')
 text = r.text
 l24 = re.findall('title>(.+?)</title', text)
 
-r = requests.get('https://tools.pingdom.com')
-text = r.text
-l25 = re.findall('title>(.+?)</title', text)
+#r = requests.get('https://tools.pingdom.com')
+#text = r.text
+#l25 = re.findall('title>(.+?)</title', text)
 
 r = requests.get('https://dnsdumpster.com')
 text = r.text
@@ -217,7 +217,7 @@ l52 = re.findall('title>(.+?)</title', text)
 
 
 
-print(l1, l2, l3, l4, l5, l6, l7,l8, l9, l10, l11, l12, l13, l14, l15, l16, l17, l18, l19, l20, l21, l22, l23, l24, l25, l26, l27, l28, l29, l30, l31, l32, l33, l34, l35, l36, l37, l38, l39, l40, l41, l42, l43, l44, l45, l46, l47, l48, l49, l50, l51, l52)
+print(l1, l2, l3, l4, l5, l6, l7,l8, l9, l10, l11, l12, l13, l14, l15, l16, l17, l18, l19, l20, l21, l22, l23, l24, l26, l27, l28, l29, l30, l31, l32, l33, l34, l35, l36, l37, l38, l39, l40, l41, l42, l43, l44, l45, l46, l47, l48, l49, l50, l51, l52)
 
 """
 4'https://fonts.googleapis.com', 5'https://techcrunch.com', 6'https://www.vice.com',
